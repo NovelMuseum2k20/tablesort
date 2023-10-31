84ebabfaf91df46e0375db8b850d55b9a292e32c
NovelMuseum2k20 <148708510+NovelMuseum2k20@users.noreply.github.com>
2023-10-31 16:54:48 +0400

    Create fsort-new.js

---
+++ fsort-new.js
@@ -0,0 +1,37 @@
+function sortTableByColumn(table, column, asc = true) {
+    const dirModifier = asc ? 1 : -1;
+    const tBody = table.tBodies[0];
+    const rows = Array.from(tBody.querySelectorAll("tr"));
+
+    const sortMethod = (a, b) => {
+        const aValue = parseFloat(a.querySelector(`td:nth-child(${column + 1})`).textContent.trim().replace('$', ''));
+        const bValue = parseFloat(b.querySelector(`td:nth-child(${column + 1})`).textContent.trim().replace('$', ''));
+        
+        return aValue > bValue ? (1 * dirModifier) : (-1 * dirModifier);
+    };
+
+    const sortedRows = rows.sort(sortMethod);
+
+    // Remove all existing TRs from the table
+    while (tBody.firstChild) {
+        tBody.removeChild(tBody.firstChild);
+    }
+
+    // Re-add the newly sorted rows
+    tBody.append(...sortedRows);
+
+    // Remember how the column is currently sorted
+    table.querySelectorAll("th").forEach(th => th.classList.remove("th-sort-asc", "th-sort-desc"));
+    table.querySelector(`th:nth-child(${column + 1})`).classList.toggle("th-sort-asc", asc);
+    table.querySelector(`th:nth-child(${column + 1})`).classList.toggle("th-sort-desc", !asc);
+}
+
+document.querySelectorAll(".table-fsortable th").forEach(headerCell => {
+    headerCell.addEventListener("click", () => {
+        const tableElement = headerCell.parentElement.parentElement.parentElement;
+        const headerIndex = Array.prototype.indexOf.call(headerCell.parentElement.children, headerCell);
+        const currentIsAscending = headerCell.classList.contains("th-sort-asc");
+
+        sortTableByColumn(tableElement, headerIndex, !currentIsAscending);
+    });
+});
